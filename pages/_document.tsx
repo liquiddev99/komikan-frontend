7967ece70446fd2afb0fafeb1874a841954f9c04
Tran Ngoc Thang <tranngocthang.bkdn@gmail.com>
2023-05-16 21:40:50 +0700

    Add google verification tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="oVr2qOSHJpPbvUVt1731vAgcnhNdcN7oEgQdC43c2dI"
+        />
+      </Head>
       <body className="bg-bg-color">
         <Main />
         <NextScript />
