69c1bd312f2f7717758b5d2764cd1bcc6e812501
Tran Ngoc Thang <tranngocthang.bkdn@gmail.com>
2023-05-20 09:08:47 +0700

    Add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Mangazine</title>
         <meta
           name="google-site-verification"
           content="oVr2qOSHJpPbvUVt1731vAgcnhNdcN7oEgQdC43c2dI"
         />
+        <meta
+          property="description"
+          content="Free and no-ads manga reading website, provide high-quality images with a comprehensive finding system, helps you easier to find your favourite manga"
+        />
+        <meta property="URL" content="https://mangazine.site/" />
+        <meta property="type" content="website" />
       </Head>
       <body className="bg-bg-color">
         <Main />
